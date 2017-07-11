432a5df1fb0c3b22f6dfa78825fcdafe118941a8
Heiner Bihlmaier <heinerbihl@gmail.com>
2017-07-11 17:46:12 +0200

    added user assign for tasks

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -12,20 +12,14 @@ export class TaskComponent implements OnInit {
   id: any;
   task: any;
   users: any;
-  usernames: any;
-  projects: any;
-  project: any;
   worker: any;
 
 
   constructor(
     private firebaseService: FirebaseService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute) { 
 
-  ) { 
-    this.usernames = {
-    }
   }
 
 
@@ -33,19 +27,14 @@ export class TaskComponent implements OnInit {
     //Get ID
     this.id = this.route.snapshot.params['id'];
 
-  this.firebaseService.getTaskDetails(this.id).subscribe(task => {
-    this.task = task;
-  });
+    this.firebaseService.getTaskDetails(this.id).subscribe(task => {
+      this.task = task;
+    });
 
+    this.firebaseService.getUsers().subscribe(users => {
+      this.users = users;
+    });
 
-    for(var i = 0; i < this.users.length; i++) {
-      if(this.project.workers.includes(this.users[i].name)) {
-        this.usernames[i] = {name: this.users[i].name};
-      }
-    }
-
-    console.log(this.project);
-    console.log(this.usernames);
   }
   
   onDeleteClick() {
@@ -54,7 +43,32 @@ export class TaskComponent implements OnInit {
     this.router.navigate(['/dashboard']);
   }
 
-  
+  onAddUser() {
+    if(this.worker != null && !this.task.workers.includes(this.worker)) {
+    let task = {
+      title: this.task.title,
+      description: this.task.description,
+      priority: this.task.priority,
+      workers: this.task.workers + ", " + this.worker,
+    }
 
+    this.firebaseService.updateTask(this.id, task);
+    }
+  }
+
+  deleteUser() {
+    if(this.worker != null && this.task.workers.includes(this.worker)) {
+      this.task.workers = this.task.workers.replace(', ' + this.worker,'');
+
+      let task = {
+        title: this.task.title,
+        description: this.task.description,
+        priority: this.task.priority,
+        workers: this.task.workers,
+      }
+
+    this.firebaseService.updateTask(this.id, task);
+    }
+  }
 
 }
